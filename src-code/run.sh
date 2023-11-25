148d1ef28f2fefc991a76353dd5f415963a822e9
jhertz <jh.furtado@campus.fct.unl.pt>
2023-11-25 01:25:59 +0000

    finished initial setup

--- src-code/run.sh
+++ src-code/run.sh
@@ -8,8 +8,8 @@ case "$1" in
     ;;
 esac
 
-echo "-> running grades-manager"
+echo "-> running interpreter-1"
 # java -jar build/libs/grades-manager-0.1.0-SNAPSHOT.jar
-java -cp bin/main/ jh.craft.interpreter.Main
+java -jar build/libs/*.jar
 
 
