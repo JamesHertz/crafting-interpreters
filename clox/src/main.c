14cb8a7bcc28f2d5e5d9497c7358ef1662c03e8f
jhertz <jh.furtado@campus.fct.unl.pt>
2024-03-03 23:21:56 +0000

    Implemented part of the input interpreter

--- clox/src/main.c
+++ clox/src/main.c
@@ -1,10 +1,54 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
 
-void repl(){
+char * read_file(char * filename){
+    FILE * file = fopen(filename, "r");
 
+    if(file == NULL){
+        fprintf(stderr, "Error opening %s: ", filename);
+        perror(""); exit(1);
+    }
+
+    struct stat st;
+    fstat(fileno(file), &st);
+
+    char * file_data = malloc( st.st_size + 1);
+    if(file_data == NULL){
+        fprintf(stderr, "Unable to reserve enough space for file %s\n", filename);
+        exit(1);
+    }
+
+    fread(file_data, st.st_size, 1, file);
+    fclose(file);
+
+    file_data[st.st_size] = 0;
+    return file_data;
 }
 
-void runFile(char * filename){
+void run_file(char * filename){
+    char * file_data = read_file(filename);
+    printf("file: %s\n\n--------\n\n: %s\n", filename, file_data);
+    free(file_data);
+}
+
+void prompt(){
+    printf("> "); fflush(stdout);
+}
+
+void repl(){
+    char line[1024];
+    for(;;){
+
+        // TODO: THIS IS UNSAFE AND I NEED TO CHANGE IT LATER c:
+        prompt();
+        if(fgets(line, 1024, stdin) == NULL){
+            putchar('\n');
+            break;
+        }
+        // do something
+        printf(">> %s\n", line);
+    }
 
 }
 
@@ -13,7 +57,7 @@ int main(int argc, char ** argv){
     if(argc == 1){
         repl();
     } else if(argc == 2){
-        runFile(argv[1]);
+        run_file(argv[1]);
     } else {
         printf("usage: %s <filename>\n", argv[0]);
     }
